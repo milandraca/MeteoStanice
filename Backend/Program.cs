2670f91cd442f7422d1c30c1bfb7b60c95e1b343
milandraca <38816321+milandraca@users.noreply.github.com>
2025-02-25 19:22:16 +0100

    ukradeni kod

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,73 +1,59 @@
 ﻿using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 
-    {
-        var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+// Add services to the container.
 
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
-    // dodati swagger
-    builder.Services.AddSwaggerGen();
+// dodati swagger
+builder.Services.AddSwaggerGen();
 
 
-    // dodavanje db contexta
-    builder.Services.AddDbContext<BackendContext>(o =>
-        {
-            o.UseSqlServer(builder.Configuration.GetConnectionString("BazaContext"));
-        });
-    builder.Services.AddCors(o =>     {
-o.AddPolicy("CorsPolicy", b => {
-b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+// dodavanje db contexta
+builder.Services.AddDbContext<EdunovaContext>(o => {
+    o.UseSqlServer(builder.Configuration.GetConnectionString("EdunovaContext"));
 });
+
+
+// Svi se od svuda na sve moguce nacine mogu spojitina naš API
+// Èitati https://code-maze.com/aspnetcore-webapi-best-practices/
+builder.Services.AddCors(o => {
+    o.AddPolicy("CorsPolicy", b => {
+        b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
-    // dodavanje servisa
-    builder.Services.AddScoped<IPersonService, PersonService>();
-    builder.Services.AddScoped<IPersonRepository, PersonRepository>();
-    var connectionString = builder.Configuration.GetConnectionString("BazaContext");
-    using (var db = new BackendContext(connectionString))
-    {
-        if (db.Database.EnsureCreated())
-        {
-            db.Persons.Add(new Person { Name = "Pero", Surname = "Perić", Age = 20 });
-            db.Persons.Add(new Person { Name = "Ivo", Surname = "Ivić", Age = 30 });
-            db.Persons.Add(new Person { Name = "Ana", Surname = "Anić", Age = 40 });
-            db.Persons.Add(new Person { Name = "Maja", Surname = "Majić", Age = 50 });
-            db.SaveChanges();
+});
 
-            var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+var app = builder.Build();
 
-            app.MapOpenApi();
+// Configure the HTTP request pipeline.
+
+app.MapOpenApi();
 
 
 
-            app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+app.UseAuthorization();
 
-            // swagger sucelje
-            app.UseSwagger();
-            app.UseSwaggerUI(o =>
-            {
-                o.EnableTryItOutByDefault();
-                o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
-            });
+// swagger sucelje
+app.UseSwagger();
+app.UseSwaggerUI(o =>
+{
+    o.EnableTryItOutByDefault();
+    o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
+});
 
-            app.MapControllers();
+app.MapControllers();
 
-            app.UseStaticFiles();
-            app.UseDefaultFiles();
-            app.MapFallbackToFile("index.html");
+app.UseStaticFiles();
+app.UseDefaultFiles();
+app.MapFallbackToFile("index.html");
 
-            app.UseCors("CorsPolicy");
+app.UseCors("CorsPolicy");
 
-
-            app.Run();
-
-        }
-    }
+app.Run();
