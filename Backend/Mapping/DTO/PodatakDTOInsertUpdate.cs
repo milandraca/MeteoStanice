880b55f76dab76ba2f8a81528c8a264d0d276ef3
milandraca <38816321+milandraca@users.noreply.github.com>
2025-03-09 15:40:10 +0100

    Backend null

--- Backend/Mapping/DTO/PodatakDTOInsertUpdate.cs
+++ Backend/Mapping/DTO/PodatakDTOInsertUpdate.cs
@@ -6,10 +6,10 @@ namespace Backend.Models.DTO
     public record PodatakDTOInsertUpdate(
         [Required(ErrorMessage = "Vrijeme obavezno")]
         DateTime Vrijeme,          
-        decimal Temperatura,
-        int BrzinaVjetra,
-        decimal RelativnaVlaga,
-        decimal KolicinaPadalina,
+        decimal? Temperatura,
+        int? BrzinaVjetra,
+        decimal? RelativnaVlaga,
+        decimal? KolicinaPadalina,
         int MeteostanicaSifra
 
 
